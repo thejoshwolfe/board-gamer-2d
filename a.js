36d212bf9a324f75ed30944377649108e5b3b5ce
Josh Wolfe <thejoshwolfe@gmail.com>
2015-08-10 17:05:35 -0700

    drag the thing with the mouse

--- a.js
+++ a.js
@@ -1,10 +1,46 @@
 var canvas = document.getElementById("canvas");
 
+var cardxloc = 100;
+var cardyloc = 100;
+var mdown = false;
+var xstart;
+var ystart;
+
 var suImage = new Image();
 suImage.src = "su.png";
 suImage.addEventListener("load", function() {
   render();
 });
+
+canvas.addEventListener("mousedown", function(event){
+  var x = eventToMouseX(event,canvas);
+  var y = eventToMouseY(event,canvas);
+  if (x > cardxloc && x<cardxloc + deckProperties.width &&
+      y > cardyloc && y<cardyloc + deckProperties.height) {
+    mdown = true;
+    xstart = x;
+    ystart = y;
+  }
+});
+document.addEventListener("mouseup", function(event){
+  mdown = false;
+});
+canvas.addEventListener("mousemove", function(event){
+  if(mdown){
+    var x = eventToMouseX(event,canvas);
+    var y = eventToMouseY(event,canvas);
+    var dx = (x - xstart);
+    var dy = (y - ystart);
+    xstart = x;
+    ystart = y;
+    cardxloc = cardxloc + dx;
+    cardyloc = cardyloc + dy;
+    render();
+  }
+});
+
+function eventToMouseX(event, canvas) { return event.clientX - canvas.getBoundingClientRect().left; }
+function eventToMouseY(event, canvas) { return event.clientY - canvas.getBoundingClientRect().top; }
 
 var cardProperties = {
   cropX: 0,
@@ -26,7 +62,7 @@ function render() {
     context.drawImage(suImage,
         cardProperties.cropX, cardProperties.cropY,
         cardProperties.cropW, cardProperties.cropH,
-        100, 100,
+        cardxloc, cardyloc,
         deckProperties.width, deckProperties.height
     );
   }
