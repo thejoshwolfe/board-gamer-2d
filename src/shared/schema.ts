ea12cde54550cc78c47430edd77d65c2a5f0cc53
Josh Wolfe <thejoshwolfe@gmail.com>
2024-01-28 01:08:13 -0500

    constraing primitive types

--- src/shared/schema.ts
+++ src/shared/schema.ts
@@ -1,80 +1,86 @@
 import {Schema} from "jsonschema";
 
-const string = {type: "string"};
-const number = {type: "number"};
-
-// TODO: These could be more constrained.
-const ImagePath = string;
-const DbEntryId = string;
-const RoleId = string;
-const ColorWithParameterizedAlpha = string;
-
-function Array(subtype: Schema) {
+function Array(subtype: Schema): Schema {
   return {type: "array", items: subtype};
 }
+function AnyOf(...subtypes: Schema[]): Schema {
+  return {anyOf: subtypes};
+}
+
+// Numbers
+export const Int: Schema = {type: "integer", minimum: -9999, maximum: 9999};
+export const UnisignedInt: Schema = {type: "integer", minimum: 0, maximum: 9999};
+export const PositiveInt: Schema = {type: "integer", minimum: 1, maximum: 9999};
+
+// Strings
+export const ImagePath: Schema = {type: "string", maxLength: 255, pattern: /^[0-9A-Za-z_-]+(\/[0-9A-Za-z_-]+)*\.png(#\d+,\d+,\d+,\d+)?$/.source};
+export const DbEntryId: Schema = {type: "string", minLength: 1, maxLength: 255};
+export const RoleId: Schema = {type: "string", minLength: 1, maxLength: 255};
+export const ColorWithParameterizedAlpha: Schema = {type: "string", maxLength: 255, pattern: /^rgba\(\d+,\d+,\d+,\$alpha\)$/.source};
+export const DisplayName: Schema = {type: "string", minLength: 1, maxLength: 255};
+export const RoomCode: Schema = {type: "string", pattern: /[A-Z]{5}/.source};
+
+export const RoomCodeOrNew: Schema = AnyOf(RoomCode, {const: "new"});
+export const EncodedMove = Array(AnyOf({type: "string"}, Int)); // TODO: this string is various id types and move codes.
 
 export const DbItem = {type: "object", additionalProperties: false, properties: {
   id: DbEntryId,
 
-  width: number,
-  height: number,
+  width: PositiveInt,
+  height: PositiveInt,
   faces: Array(ImagePath),
 
   snapZones: Array({
     type: "object", additionalProperties: false, properties: {
-      x: number,
-      y: number,
-      width: number,
-      height: number,
-      cellWidth: number,
-      cellHeight: number,
+      x: Int,
+      y: Int,
+      width: PositiveInt,
+      height: PositiveInt,
+      cellWidth: PositiveInt,
+      cellHeight: PositiveInt,
     },
   }),
 
-  closetName: string,
+  closetName: DisplayName,
   thumbnail: ImagePath,
-  thumbnailWidth: number,
-  thumbnailHeight: number,
+  thumbnailWidth: PositiveInt,
+  thumbnailHeight: PositiveInt,
   items: Array(DbEntryId),
 
-  hideFaces: Array(number),
+  hideFaces: Array(UnisignedInt),
   visionWhitelist: Array(RoleId),
   labelPlayerName: RoleId,
   backgroundColor: ColorWithParameterizedAlpha,
 }, required: ["id"]};
 
-export const protocolSchema: Schema = {
-  anyOf: [
+export const protocolSchema: Schema = AnyOf(
+  { type: "object", additionalProperties: false, properties: {
+    cmd: {const: "joinRoom"},
+    args: {type: "object", additionalProperties: false, properties: {
+      roomCode: RoomCodeOrNew,
+    }, required: ["roomCode"]},
+  }, required: ["cmd", "args"]},
 
-    { type: "object", additionalProperties: false, properties: {
-      cmd: {const: "joinRoom"},
-      args: {type: "object", additionalProperties: false, properties: {
-        roomCode: string,
-      }, required: ["roomCode"]},
-    }, required: ["cmd", "args"]},
+  { type: "object", additionalProperties: false, properties: {
+    cmd: {const: "changeMyName"},
+    args: DisplayName,
+  }, required: ["cmd", "args"]},
 
-    { type: "object", additionalProperties: false, properties: {
-      cmd: {const: "changeMyName"},
-      args: string,
-    }, required: ["cmd", "args"]},
+  { type: "object", additionalProperties: false, properties: {
+    cmd: {const: "changeMyRole"},
+    args: AnyOf(RoleId, {const: ""}),
+  }, required: ["cmd", "args"]},
 
-    { type: "object", additionalProperties: false, properties: {
-      cmd: {const: "changeMyRole"},
-      args: string,
-    }, required: ["cmd", "args"]},
+  { type: "object", additionalProperties: false, properties: {
+    cmd: {const: "makeAMove"},
+    args: EncodedMove,
+  }, required: ["cmd", "args"]},
 
-    { type: "object", additionalProperties: false, properties: {
-      cmd: {const: "makeAMove"},
-      args: Array({anyOf: [string, number]}),
-    }, required: ["cmd", "args"]},
-
-    { type: "object", additionalProperties: false, properties: {
-      cmd: {const: "putDbEntry"},
-      args: DbItem,
-    }, required: ["cmd", "args"]},
-
-  ],
-};
+  { type: "object", additionalProperties: false, properties: {
+    cmd: {const: "putDbEntry"},
+    args: DbItem,
+  }, required: ["cmd", "args"]},
+);
 
 // Rewrite references to our exported schemas to use {$ref: "#/$defs/TypeName"}.
 // This causes the output file to contain multiple export declarations.
